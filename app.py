9c7c66e0cd48634161c3ae5acfcb5dc2a323c645
Sourabh Shekhar Singh <sourabh@doxel.ai>
2024-02-29 12:44:11 +0530

    add new

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from flask import Flask, render_template
+print(__name__)
+app = Flask(__name__)
+@app.route("/")
+
+def hello_world():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
